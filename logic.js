a527af6db90ba270a1b3213807f3b7329315568d
anil093 <anil.kumar093@outlook.com>
2018-11-27 13:33:50 -0500

    changes added

--- logic.js
+++ logic.js
@@ -1,268 +1,253 @@
-window.onload = function () {
-  var alphabet = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
-  ]
+window.onload = function() {
+	var alphabet = [
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
+		'f',
+		'g',
+		'h',
+		'i',
+		'j',
+		'k',
+		'l',
+		'm',
+		'n',
+		'o',
+		'p',
+		'q',
+		'r',
+		's',
+		't',
+		'u',
+		'v',
+		'w',
+		'x',
+		'y',
+		'z'
+	];
 
-  var categories // Array of topics
-  var chosenCategory // Selected catagory
-  var getHint // Word getHint
-  var word // Selected word
-  var guess // Geuss
-  var geusses = [] // Stored geusses
-  var lives // Lives
-  var counter // Count correct geusses
-  var space // Number of spaces in word '-'
+	var categories;
+	var Selectedcatagory;
+	var Selectedword;
+	var guess;
+	var geusses = [];
+	var lives;
+	var correctgeusses;
+	var Numberofspacesinword;
 
-    // Get elements
-  var showLives = document.getElementById('mylives')
-  var showCatagory = document.getElementById('scatagory')
-  var getHint = document.getElementById('hint')
-  var showClue = document.getElementById('clue')
+	var howmanyliveleft = document.getElementById('mylives');
+	var whatistheClue = document.getElementById('clue');
 
-    // create alphabet ul
-  var buttons = function () {
-    myButtons = document.getElementById('buttons')
-    letters = document.createElement('ul')
+	var playbuttons = function() {
+		hangamnButtons = document.getElementById('buttons');
+		letters = document.createElement('ul');
 
-    for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet'
-      list = document.createElement('li')
-      list.id = 'letter'
-      list.innerHTML = alphabet[i]
-      check()
-      myButtons.appendChild(letters)
-      letters.appendChild(list)
-    }
-  }
+		for (var i = 0; i < alphabet.length; i++) {
+			letters.id = 'alphabet';
+			list = document.createElement('li');
+			list.id = 'letter';
+			list.innerHTML = alphabet[i];
+			check();
+			hangamnButtons.appendChild(letters);
+			letters.appendChild(list);
+		}
+	};
 
-    // Select Catagory
-  var selectCat = function () {
-    if (chosenCategory === categories[0]) {
-      catagoryName.innerHTML = 'The Chosen Category Is Premier League Football Teams'
-    } else if (chosenCategory === categories[1]) {
-      catagoryName.innerHTML = 'The Chosen Category Is Films'
-    } else if (chosenCategory === categories[2]) {
-      catagoryName.innerHTML = 'The Chosen Category Is Cities'
-    }
-  }
+	var selectCat = function() {
+		if (Selectedcatagory === categories[0]) {
+			catagoryName.innerHTML = 'The Chosen Category Is Premier League Football Teams';
+		} else if (Selectedcatagory === categories[1]) {
+			catagoryName.innerHTML = 'The Chosen Category Is Films';
+		} else if (Selectedcatagory === categories[2]) {
+			catagoryName.innerHTML = 'The Chosen Category Is Cities';
+		}
+	};
 
-    // Create geusses ul
-  result = function () {
-    wordHolder = document.getElementById('hold')
-    correct = document.createElement('ul')
+	result = function() {
+		wordHolder = document.getElementById('hold');
+		correct = document.createElement('ul');
 
-    for (var i = 0; i < word.length; i++) {
-      correct.setAttribute('id', 'my-word')
-      guess = document.createElement('li')
-      guess.setAttribute('class', 'guess')
-      if (word[i] === '-') {
-        guess.innerHTML = '-'
-        space = 1
-      } else {
-        guess.innerHTML = '_'
-      }
+		for (var i = 0; i < Selectedword.length; i++) {
+			correct.setAttribute('id', 'my-word');
 
-      geusses.push(guess)
-      wordHolder.appendChild(correct)
-      correct.appendChild(guess)
-    }
-  }
+			guess = document.createElement('li');
+			guess.setAttribute('class', 'guess');
+			if (Selectedword[i] === '-') {
+				guess.innerHTML = '-';
+				Numberofspacesinword = 1;
+			} else {
+				guess.innerHTML = '_';
+			}
 
-    // Show lives
-  comments = function () {
-    showLives.innerHTML = 'You have ' + lives + ' lives'
-    if (lives < 1) {
-      showLives.innerHTML = 'Game Over'
-    }
-    for (var i = 0; i < geusses.length; i++) {
-      if (counter + space === geusses.length) {
-        showLives.innerHTML = 'You Win!'
-      }
-    }
-  }
+			geusses.push(guess);
+			wordHolder.appendChild(correct);
+			correct.appendChild(guess);
+		}
+	};
 
-    // Animate man
-  var animate = function () {
-    var drawMe = lives
-    drawArray[drawMe]()
-  }
+	Showlives = function() {
+		howmanyliveleft.innerHTML = 'I am Gonna Die Only ' + lives + ' lives';
+		if (lives < 1) {
+			howmanyliveleft.innerHTML = 'Game Over Loser! Hard Luck';
+		}
+		for (var i = 0; i < geusses.length; i++) {
+			if (correctgeusses + Numberofspacesinword === geusses.length) {
+				howmanyliveleft.innerHTML = ' You won Congrats!';
+			}
+		}
+	};
 
-    // Hangman
-  canvas = function () {
-    myStickman = document.getElementById('stickman')
-    context = myStickman.getContext('2d')
-    context.beginPath()
-    context.strokeStyle = '#fff'
-    context.lineWidth = 2
-  }
+	var drawman = function() {
+		var drawMe = lives;
+		drawArray[drawMe]();
+	};
 
-  head = function () {
-    myStickman = document.getElementById('stickman')
-    context = myStickman.getContext('2d')
-    context.beginPath()
-    context.arc(60, 25, 10, 0, Math.PI * 2, true)
-    context.stroke()
-  }
+	canvas = function() {
+		myStickman = document.getElementById('Hangman');
+		context = myStickman.getContext('2d');
+		context.beginPath();
+		context.strokeStyle = '#fff';
+		context.lineWidth = 2;
+	};
 
-  draw = function ($pathFromx, $pathFromy, $pathTox, $pathToy) {
-    context.moveTo($pathFromx, $pathFromy)
-    context.lineTo($pathTox, $pathToy)
-    context.stroke()
-  }
+	head = function() {
+		myStickman = document.getElementById('Hangman');
+		context = myStickman.getContext('2d');
+		context.beginPath();
+		context.arc(60, 25, 10, 0, Math.PI * 2, true);
+		context.stroke();
+	};
 
-  frame1 = function () {
-    draw(0, 150, 150, 150)
-  }
+	draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+		context.moveTo($pathFromx, $pathFromy);
+		context.lineTo($pathTox, $pathToy);
+		context.stroke();
+	};
 
-  frame2 = function () {
-    draw(10, 0, 10, 600)
-  }
+	belowline = function() {
+		draw(0, 150, 150, 150);
+	};
 
-  frame3 = function () {
-    draw(0, 5, 70, 5)
-  }
+	aboveline = function() {
+		draw(10, 0, 10, 600);
+	};
 
-  frame4 = function () {
-    draw(60, 5, 60, 15)
-  }
+	leftline = function() {
+		draw(0, 5, 70, 5);
+	};
 
-  torso = function () {
-    draw(60, 36, 60, 70)
-  }
+	rigthline = function() {
+		draw(60, 5, 60, 15);
+	};
 
-  rightArm = function () {
-    draw(60, 46, 100, 50)
-  }
+	torso = function() {
+		draw(60, 36, 60, 70);
+	};
 
-  leftArm = function () {
-    draw(60, 46, 20, 50)
-  }
+	rightArm = function() {
+		draw(60, 46, 100, 50);
+	};
 
-  rightLeg = function () {
-    draw(60, 70, 100, 100)
-  }
+	leftArm = function() {
+		draw(60, 46, 20, 50);
+	};
 
-  leftLeg = function () {
-    draw(60, 70, 20, 100)
-  }
+	rightLeg = function() {
+		draw(60, 70, 100, 100);
+	};
 
-  drawArray = [rightLeg, leftLeg, rightArm, leftArm, torso, head, frame4, frame3, frame2, frame1]
+	leftLeg = function() {
+		draw(60, 70, 20, 100);
+	};
 
-    // OnClick Function
-  check = function () {
-    list.onclick = function () {
-      var geuss = this.innerHTML
-      this.setAttribute('class', 'active')
-      this.onclick = null
-      for (var i = 0; i < word.length; i++) {
-        if (word[i] === geuss) {
-          geusses[i].innerHTML = geuss
-          counter += 1
-        }
-      }
-      var j = word.indexOf(geuss)
-      if (j === -1) {
-        lives -= 1
-        comments()
-        animate()
-      } else {
-        comments()
-      }
-    }
-  }
+	drawArray = [ rightLeg, leftLeg, rightArm, leftArm, torso, head, belowline, aboveline, leftline, rigthline ];
 
-    // Play
-  play = function () {
-    categories = [
-            ['everton', 'liverpool', 'swansea', 'chelsea', 'hull', 'manchester-city', 'newcastle-united'],
-            ['alien', 'dirty-harry', 'gladiator', 'finding-nemo', 'jaws'],
-            ['manchester', 'milan', 'madrid', 'amsterdam', 'prague']
-    ]
+	check = function() {
+		list.onclick = function() {
+			var geuss = this.innerHTML;
+			this.setAttribute('class', 'active');
+			this.onclick = null;
+			for (var i = 0; i < Selectedword.length; i++) {
+				if (Selectedword[i] === geuss) {
+					geusses[i].innerHTML = geuss;
+					correctgeusses += 1;
+				}
+			}
+			var j = Selectedword.indexOf(geuss);
+			if (j === -1) {
+				lives -= 1;
+				Showlives();
+				drawman();
+			} else {
+				Showlives();
+			}
+		};
+	};
 
-    chosenCategory = categories[Math.floor(Math.random() * categories.length)]
-    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)]
-    word = word.replace(/\s/g, '-')
-    console.log(word)
-    buttons()
+	play = function() {
+		categories = [
+			[ 'everton', 'liverpool', 'swansea', 'chelsea', 'hull', 'manchester-city', 'newcastle-united' ],
+			[ 'alien', 'dirty-harry', 'gladiator', 'finding-nemo', 'jaws' ],
+			[ 'manchester', 'milan', 'madrid', 'amsterdam', 'prague' ]
+		];
 
-    geusses = []
-    lives = 10
-    counter = 0
-    space = 0
-    result()
-    comments()
-    selectCat()
-    canvas()
-  }
+		Selectedcatagory = categories[Math.floor(Math.random() * categories.length)];
+		Selectedword = Selectedcatagory[Math.floor(Math.random() * Selectedcatagory.length)];
+		Selectedword = Selectedword.replace(/\s/g, '-');
+		console.log(Selectedword);
+		playbuttons();
 
-  play()
+		geusses = [];
+		lives = 10;
+		correctgeusses = 0;
+		Numberofspacesinword = 0;
+		result();
+		Showlives();
+		selectCat();
+		canvas();
+	};
 
-    // Hint
+	play();
 
-  hint.onclick = function () {
-    hints = [
-      [
-        'Based in Mersyside',
-        'Based in Mersyside',
-        'First Welsh team to reach the Premier Leauge',
-        'Owned by A russian Billionaire',
-        'Once managed by Phil Brown',
-        '2013 FA Cup runners up',
-        "Gazza's first club"
-      ],
-      [
-        'Science-Fiction horror film',
-        '1971 American action film',
-        'Historical drama',
-        'Anamated Fish',
-        'Giant great white shark'
-      ],
-      [
-        'Northern city in the UK',
-        'Home of AC and Inter',
-        'Spanish capital',
-        'Netherlands capital',
-        'Czech Republic capital'
-      ]
-    ]
+	hint.onclick = function() {
+		hints = [
+			[
+				'Based in Mersyside',
+				'Based in Mersyside',
+				'First Welsh team to reach the Premier Leauge',
+				'Owned by A russian Billionaire',
+				'Once managed by Phil Brown',
+				'2013 FA Cup runners up',
+				"Gazza's first club"
+			],
+			[
+				'Science-Fiction horror film',
+				'1971 American action film',
+				'Historical drama',
+				'Anamated Fish',
+				'Giant great white shark'
+			],
+			[
+				'Northern city in the UK',
+				'Home of AC and Inter',
+				'Spanish capital',
+				'Netherlands capital',
+				'Czech Republic capital'
+			]
+		];
 
-    var catagoryIndex = categories.indexOf(chosenCategory)
-    var hintIndex = chosenCategory.indexOf(word)
-    showClue.innerHTML = 'Clue: - ' + hints[catagoryIndex][hintIndex]
-  }
+		var catagoryIndex = categories.indexOf(Selectedcatagory);
+		var hintIndex = Selectedcatagory.indexOf(Selectedword);
+		whatistheClue.innerHTML = 'Clue: - ' + hints[catagoryIndex][hintIndex];
+	};
 
-    // Reset
-
-  document.getElementById('reset').onclick = function () {
-    correct.parentNode.removeChild(correct)
-    letters.parentNode.removeChild(letters)
-    showClue.innerHTML = ''
-    context.clearRect(0, 0, 400, 400)
-    play()
-  }
-}
+	document.getElementById('reset').onclick = function() {
+		correct.parentNode.removeChild(correct);
+		letters.parentNode.removeChild(letters);
+		whatistheClue.innerHTML = '';
+		context.clearRect(0, 0, 400, 400);
+		play();
+	};
+};
